893ad86facaaa755049fda2316f74542be769765
Josh Alling <joshralling@gmail.com>
2025-03-15 09:12:15 -0700

    split nodes

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,42 +1,42 @@
 import unittest
 
-from utils import text_node_to_html_node
+from utils import text_node_to_html_node, split_nodes_delimiter
 from textnode import TextNode, TextType
 
 
 class TestUtils(unittest.TestCase):
-    def test_text(self):
+    def test_node_to_html_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
-    def test_bold(self):
+    def test_node_to_html_bold(self):
         node = TextNode("This is a bold node", TextType.BOLD)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is a bold node")
 
-    def test_italic(self):
+    def test_node_to_html_italic(self):
         node = TextNode("This is an italic node", TextType.ITALIC)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "i")
         self.assertEqual(html_node.value, "This is an italic node")
 
-    def test_code(self):
+    def test_node_to_html_code(self):
         node = TextNode("This is a code node", TextType.CODE)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "code")
         self.assertEqual(html_node.value, "This is a code node")
 
-    def test_link(self):
+    def test_node_to_html_link(self):
         node = TextNode("This is a link node", TextType.LINK, "example.com")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "a")
         self.assertEqual(html_node.value, "This is a link node")
         self.assertEqual(html_node.props, {"href": "example.com"})
 
-    def test_image(self):
+    def test_node_to_html_image(self):
         node = TextNode("This is an image node", TextType.IMAGE, "example.com")
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "img")
@@ -44,3 +44,30 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(
             html_node.props, {"src": "example.com", "alt": "This is an image node"}
         )
+
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+
+    def test_split_nodes_delimiter_with_many_old_nodes(self):
+        node_1 = TextNode("This is text with a ", TextType.TEXT)
+        node_2 = TextNode("code block", TextType.CODE)
+        node_3 = TextNode(" word and **bold** text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node_1, node_2, node_3], "**", TextType.BOLD)
+        self.assertEqual(new_nodes[0].text, "This is text with a ")
+        self.assertEqual(new_nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[1].text, "code block")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[2].text, " word and ")
+        self.assertEqual(new_nodes[2].text_type, TextType.TEXT)
+        self.assertEqual(new_nodes[3].text, "bold")
+        self.assertEqual(new_nodes[3].text_type, TextType.BOLD)
+        self.assertEqual(new_nodes[4].text, " text")
+        self.assertEqual(new_nodes[4].text_type, TextType.TEXT)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-from textnode import TextType
+from textnode import TextType, TextNode
 from leafnode import LeafNode
 
 
@@ -18,3 +18,13 @@ def text_node_to_html_node(textnode):
             )
         case _:
             return LeafNode(None, textnode.text)
+
+
+def split_nodes_delimiter(nodes, delimiter, text_type):
+    new_nodes = []
+    for node in nodes:
+        text_list = node.text.split(delimiter)
+        for i in range(len(text_list)):
+            type = i % 2 == 1 and text_type or node.text_type
+            new_nodes.append(TextNode(text_list[i], type))
+    return new_nodes
