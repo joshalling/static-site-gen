389c72394f1d34d3abb586c033909971ce4d6512
Josh Alling <joshralling@gmail.com>
2025-03-21 17:28:27 -0700

    block to block type

--- src/blockutils.py
+++ src/blockutils.py
@@ -1,3 +1,32 @@
+from enum import Enum
+import re
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    UNORDERED_LIST = "ulist"
+    ORDERED_LIST = "olist"
+    CODE = "code"
+    QUOTE = "quote"
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     return list(map(lambda block: block.strip(), blocks))
+
+
+def block_to_blocktype(block):
+    lines = block.split("\n")
+    if len(lines) == 1 and re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    elif all(line.startswith("- ") for line in lines):
+        return BlockType.UNORDERED_LIST
+    elif all(re.match(r"^\d+\. ", line) for line in lines):
+        return BlockType.ORDERED_LIST
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    else:
+        return BlockType.PARAGRAPH
--- src/test_blockutils.py
+++ src/test_blockutils.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blockutils import markdown_to_blocks
+from blockutils import markdown_to_blocks, block_to_blocktype, BlockType
 
 
 class TestBlockUtils(unittest.TestCase):
@@ -23,3 +23,67 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type_heading(self):
+        block = """### Heading"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_invalid(self):
+        block = """####### Heading"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_unordered_list(self):
+        block = """- item1\n- item2\n- item3"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_unordered_list_invalid(self):
+        block = """- item1\n-item2\n- item3"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = """1. item1\n1. item2\n1. item3"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.ORDERED_LIST)
+
+    def test_block_to_block_type_ordered_list_invalid(self):
+        block = """1. item1\n1 item2\n1. item3"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_code(self):
+        block = """```code
+        is
+        cool```"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.CODE)
+
+    def test_block_to_block_type_code_invalid(self):
+        block = """```code
+        is
+        cool``"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_quote(self):
+        block = """> quote1\n> quote2\n> quote3"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.QUOTE)
+
+    def test_block_to_block_type_quote_invalid(self):
+        block = """> quote1\n- quote2\n> quote3"""
+        type = block_to_blocktype(block)
+
+        self.assertEqual(type, BlockType.PARAGRAPH)
