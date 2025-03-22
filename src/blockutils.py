6bcf2e16fa33e0f78c50e7dad8638684de7e9076
Josh Alling <joshralling@gmail.com>
2025-03-21 21:39:20 -0700

    markdown to html

--- src/blockutils.py
+++ src/blockutils.py
@@ -13,6 +13,10 @@ class BlockType(Enum):
     QUOTE = "quote"
 
 
+heading_regex = r"^#{1,6} "
+olist_regex = r"^\d+\. "
+
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     return list(
@@ -22,7 +26,7 @@ def markdown_to_blocks(markdown):
 
 def block_to_blocktype(block):
     lines = block.split("\n")
-    if len(lines) == 1 and re.match(r"^#{1,6} ", block):
+    if len(lines) == 1 and re.match(heading_regex, block):
         return BlockType.HEADING
     elif all(line.startswith("- ") for line in lines):
         return BlockType.UNORDERED_LIST
@@ -39,24 +43,80 @@ def block_to_blocktype(block):
 def split_block_of_type(block, blocktype):
     match blocktype:
         case BlockType.HEADING:
-            prefix = re.findall(r"^#{1,6} ", block)[0]
+            prefix = re.findall(heading_regex, block)[0]
             return block[: len(prefix) - 1], block[len(prefix) :]
+        case BlockType.UNORDERED_LIST:
+            return None, block[2:]
+        case BlockType.ORDERED_LIST:
+            prefix = re.findall(olist_regex, block)[0]
+            return None, block[len(prefix) :]
+        case BlockType.CODE:
+            return None, block[3 : len(block) - 3]
+        case BlockType.QUOTE:
+            return None, block[2:]
+        case BlockType.PARAGRAPH:
+            return None, block
 
 
 def blocktype_to_html_tag(blocktype, prefix):
     match blocktype:
         case BlockType.HEADING:
             return f"h{len(prefix)}"
+        case BlockType.UNORDERED_LIST:
+            return "ul"
+        case BlockType.ORDERED_LIST:
+            return "ol"
+        case BlockType.CODE:
+            return "pre"
+        case BlockType.QUOTE:
+            return "blockquote"
+        case BlockType.PARAGRAPH:
+            return "p"
 
 
-def block_to_html(block):
-    blocktype = block_to_blocktype(block)
+def get_block_meta(block, blocktype):
     prefix, text = split_block_of_type(block, blocktype)
 
     textnodes = inlineutils.text_to_text_nodes(text)
     inner = "".join(
-        map(lambda node: inlineutils.text_node_to_html_node(node).to_html(), textnodes)
+        map(
+            lambda node: inlineutils.text_node_to_html_node(node).to_html(),
+            textnodes,
+        )
     )
     tag = blocktype_to_html_tag(blocktype, prefix)
 
-    return f"<{tag}>{inner}</{tag}>"
+    return inner, tag
+
+
+def block_to_html(block):
+    blocktype = block_to_blocktype(block)
+    html_lines = []
+    tag = None
+    if blocktype == BlockType.HEADING:
+        inner, tag = get_block_meta(block, blocktype)
+        html_lines.append(inner)
+    elif blocktype == BlockType.CODE:
+        prefix, text = split_block_of_type(block, blocktype)
+        tag = blocktype_to_html_tag(blocktype, prefix)
+        html_lines.append(text)
+    else:
+        lines = block.split("\n")
+        for line in lines:
+            inner, tag = get_block_meta(line, blocktype)
+            if (
+                blocktype == BlockType.ORDERED_LIST
+                or blocktype == BlockType.UNORDERED_LIST
+            ):
+                html_lines.append(f"<li>{inner}</li>")
+                continue
+            html_lines.append(inner)
+    delimiter = " " if blocktype in [BlockType.QUOTE, BlockType.PARAGRAPH] else ""
+
+    return f"<{tag}>{delimiter.join(html_lines)}</{tag}>"
+
+
+def markdown_to_html(markdown):
+    blocks = markdown_to_blocks(markdown)
+    html_blocks = list(map(lambda block: block_to_html(block), blocks))
+    return f"<div>{"".join(html_blocks)}</div>"
--- src/test_blockutils.py
+++ src/test_blockutils.py
@@ -116,6 +116,40 @@ This is the same paragraph on a new line
             blockutils.split_block_of_type("### Head", blockutils.BlockType.HEADING),
             ("###", "Head"),
         )
+        self.assertEqual(
+            blockutils.split_block_of_type(
+                "> Quote **BOLD**", blockutils.BlockType.QUOTE
+            ),
+            (None, "Quote **BOLD**"),
+        )
+        self.assertEqual(
+            blockutils.split_block_of_type(
+                "- unordered list item", blockutils.BlockType.UNORDERED_LIST
+            ),
+            (None, "unordered list item"),
+        )
+        self.assertEqual(
+            blockutils.split_block_of_type(
+                "123. ordered list item", blockutils.BlockType.ORDERED_LIST
+            ),
+            (None, "ordered list item"),
+        )
+        self.assertEqual(
+            blockutils.split_block_of_type(
+                "```this is some preformatted code```", blockutils.BlockType.CODE
+            ),
+            (None, "this is some preformatted code"),
+        )
+        self.assertEqual(
+            blockutils.split_block_of_type(
+                "This is a paragraph of text with a line brea.\nThis is after the line break",
+                blockutils.BlockType.PARAGRAPH,
+            ),
+            (
+                None,
+                "This is a paragraph of text with a line brea.\nThis is after the line break",
+            ),
+        )
 
     def test_block_to_html_heading(self):
         block = "##### This is a _small_ heading with `code` in it"
@@ -125,3 +159,84 @@ This is the same paragraph on a new line
             "<h5>This is a <i>small</i> heading with <code>code</code> in it</h5>",
             result,
         )
+
+    def test_block_to_html_ordered_list(self):
+        block = "1. ordered **item** 1\n1. _ordered_ item 2"
+        result = blockutils.block_to_html(block)
+
+        self.assertEqual(
+            "<ol><li>ordered <b>item</b> 1</li><li><i>ordered</i> item 2</li></ol>",
+            result,
+        )
+
+    def test_block_to_html_unordered_list(self):
+        block = "- ordered **item** 1\n- _ordered_ item 2"
+        result = blockutils.block_to_html(block)
+
+        self.assertEqual(
+            "<ul><li>ordered <b>item</b> 1</li><li><i>ordered</i> item 2</li></ul>",
+            result,
+        )
+
+    def test_block_to_html_code(self):
+        block = """```
+        // This **is** _some_ preformatted code
+        def funky_func():
+            pass
+        ```"""
+        result = blockutils.block_to_html(block)
+
+        self.assertEqual(
+            """<pre>
+        // This **is** _some_ preformatted code
+        def funky_func():
+            pass
+        </pre>""",
+            result,
+        )
+
+    def test_block_to_html_quote(self):
+        block = "> This is a quote\n> on two lines"
+        result = blockutils.block_to_html(block)
+
+        self.assertEqual(
+            "<blockquote>This is a quote on two lines</blockquote>",
+            result,
+        )
+
+    def test_block_to_html_paragraph(self):
+        block = "This is a **long** paragraph\nwith multiple lines of text."
+        result = blockutils.block_to_html(block)
+
+        self.assertEqual(
+            "<p>This is a <b>long</b> paragraph with multiple lines of text.</p>",
+            result,
+        )
+
+    def test_markdown_to_html(self):
+        markdown = """# Heading 1
+
+This is a description but we added some **bold** and _italic_ words.
+
+## This is the first subheading
+
+```
+// we should include some code
+def my_func():
+    print("do something")
+```
+
+- item 1
+- item `2`
+
+1. numbered 1
+2987987. _numbered_ 2
+
+> This is a blockquote
+> with 2 lines
+"""
+        result = blockutils.markdown_to_html(markdown)
+
+        expected = '<div><h1>Heading 1</h1><p>This is a description but we added some <b>bold</b> and <i>italic</i> words.</p><h2>This is the first subheading</h2><pre>\n// we should include some code\ndef my_func():\n    print("do something")\n</pre><ul><li>item 1</li><li>item <code>2</code></li></ul><ol><li>numbered 1</li><li><i>numbered</i> 2</li></ol><blockquote>This is a blockquote with 2 lines</blockquote></div>'
+
+        self.assertEqual(result, expected)
